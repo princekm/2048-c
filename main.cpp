84e8614fd7a504741a90f6fa32ef36de8e46cb90
princekm <prince.k.m.005@gmail.com>
2017-06-09 21:37:23 +0530

    game options added

--- main.cpp
+++ main.cpp
@@ -2,10 +2,25 @@
 #include<iostream>
 using namespace std;
 
-
+void backspace(int count){
+    int i;
+    for(i=0;i<count;++i)
+        printf("\b \b");
+}
 int main(){
     char key;
     matrix m;
+    cout<<"   ----------------\n";
+    cout<<"         2048\n";
+    cout<<"   ----------------\n";
+    cout<<"          w\n";
+    cout<<"          ^\n";
+    cout<<"          |\n";
+    cout<<"     a <-- --> d\n";
+    cout<<"          |\n";
+    cout<<"          v\n";
+    cout<<"          s\n";
+
     m.fill_random();
     m.display();
     bool generate;
@@ -18,8 +33,9 @@ int main(){
             case KEY_LEFT:generate = m.left();break;
             case KEY_RIGHT:generate = m.right();break;
         }
-        if(generate)
+        if(generate){
             m.fill_random();
+        }
         else if(m.no_zeroes()){
             cout<<"GAME OVER";
             exit(0);
